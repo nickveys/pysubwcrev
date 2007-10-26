ff4d7e15a4b7515dde2ae129ab1ba1b3a5ae4730
psylence519 <psylence519@1058642b-333d-0410-854a-593517148259>
2007-10-26 06:41:59 +0000

    added license, slick dictionary construction
    git-svn-id: https://pysubwcrev.googlecode.com/svn/trunk@7 1058642b-333d-0410-854a-593517148259

--- src/app.py
+++ src/app.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+#
+# This file is part of pysubwcrev.
+#
+# pysubwcrev is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pysubwcrev is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pysubwcrev.  If not, see <http://www.gnu.org/licenses/>.
+
 from optparse import OptionParser
 from time import strftime, gmtime
 import os, pysvn, re, sys
@@ -43,14 +60,15 @@ def gather(workingCopyDir, opts):
         wcrange = "%s" % (maxrev)
         isMixed = False
 
-    results = {}
-    results['wcrange'] = wcrange
-    results['wcmixed'] = isMixed
-    results['wcmods']  = hasMods
-    results['wcrev']   = maxrev
-    results['wcurl']   = client.info(workingCopyDir).url
-    results['wcdate']  = strftime("%Y-%m-%d %H:%M:%S", gmtime(maxdate))
-    results['wcnow']   = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    results = {
+        'wcrange' : wcrange,
+        'wcmixed' : isMixed,
+        'wcmods'  : hasMods,
+        'wcrev'   : maxrev,
+        'wcurl'   : client.info(workingCopyDir).url,
+        'wcdate'  : strftime("%Y-%m-%d %H:%M:%S", gmtime(maxdate)),
+        'wcnow'   : strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    }
 
     #print results
     return results
